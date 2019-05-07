7ade0560445653ff6360fb2ca1beb575704c6544
abused_master <techperson71@gmail.com>
2019-05-07 07:00:30 -0400

    Fix solar panels generating during the day

--- src/main/java/abused_master/refinedmachinery/tiles/generator/BlockEntitySolarPanel.java
+++ src/main/java/abused_master/refinedmachinery/tiles/generator/BlockEntitySolarPanel.java
@@ -6,6 +6,7 @@ import abused_master.refinedmachinery.registry.ModBlockEntities;
 import abused_master.refinedmachinery.utils.EnergyHelper;
 import nerdhub.cardinalenergy.api.IEnergyHandler;
 import nerdhub.cardinalenergy.impl.EnergyStorage;
+import net.minecraft.block.GlassBlock;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -37,7 +38,7 @@ public class BlockEntitySolarPanel extends BlockEntityBase implements IEnergyHan
 
     @Override
     public void tick() {
-        if(world.isDaylight()) {
+        if(world.isDaylight() && canSeeSky()) {
             if((storage.getEnergyStored() + generationPerTick) < storage.getEnergyCapacity()) {
                 storage.receiveEnergy(generationPerTick);
             }
@@ -45,6 +46,21 @@ public class BlockEntitySolarPanel extends BlockEntityBase implements IEnergyHan
 
         EnergyHelper.sendEnergy(storage, world, pos, generationPerTick);
         this.markDirty();
+    }
+
+    public boolean canSeeSky() {
+        int height = 256 - pos.getY();
+
+        for (int i = 1; i < height; i++) {
+            BlockPos blockPos = new BlockPos(pos.getX(), pos.getY() + i, pos.getZ());
+            if(world.isHeightInvalid(blockPos) || !world.isAir(blockPos)) {
+                return false;
+            }
+
+            continue;
+        }
+
+        return true;
     }
 
     public void setType(EnumSolarPanelTypes type) {
